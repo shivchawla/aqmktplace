d0e546a1e5b9c47bc0c75d554b728cebf45fcb5b
Shiv Chawla <shivchawla2001@gmail.com>
2018-05-19 16:54:17 +0530

    Updated tooltip text

--- src/containers/PortfolioDetail.jsx
+++ src/containers/PortfolioDetail.jsx
@@ -358,12 +358,12 @@ class PortfolioDetailImpl extends React.Component {
                 const netValue = _.get(portfolioMetrics, 'netValue', null);
 
                 const metrics = [
-                    {value: annualReturn, label: annualReturnLabel, fixed: 2, percentage: true, tooltipText: 'Compounded annual growth rate over last year'},
+                    {value: annualReturn, label: annualReturnLabel, fixed: 2, percentage: true, tooltipText: 'Compounded annual growth rate since inception'},
                     {value: volatility, label: volatilityLabel, fixed: 2, percentage: true, tooltipText: 'Annualized standard deviation of daily returns since inception'},
                     {value: totalReturn, label: 'Total Return', fixed: 2, percentage: true, color: true, tooltipText: 'Total return since inception'},
                     //{value: dailyNAVChangePct, label: dailyNAVChangePctLabel, fixed: 2, percentage: true, color: true, direction:true},
-                    {value: totalPnl, label: unrealizedPnlLabel, fixed: 2, money:true, color: true, direction:true, tooltipText: 'Unrealized PNL'},
-                    {value: netValue, label: netValueLabel, isNetValue: true, money:true, dailyChangePct: dailyNAVChangePct, fixed: Math.round(netValue) == netValue ? 0 : 2, tooltip: 'Net Value of Portfolio'}
+                    {value: totalPnl, label: unrealizedPnlLabel, fixed: 2, money:true, color: true, direction:true, tooltipText: 'Unrealized Profit/Loss'},
+                    {value: netValue, label: netValueLabel, isNetValue: true, money:true, dailyChangePct: dailyNAVChangePct, fixed: Math.round(netValue) == netValue ? 0 : 2, tooltipText: 'Net Value of the Portfolio'}
                 ];
                 this.setState({
                     portfolioMetrics: metrics,
