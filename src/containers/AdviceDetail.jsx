f98b27626760e3a3c9a1783ba66583602583752f
Saurav Biswas <saru.sreyo@gmail.com>
2018-06-11 13:12:12 +0530

    Fixed url for UpdateAdvice in AdviceDetail

--- src/containers/AdviceDetail.jsx
+++ src/containers/AdviceDetail.jsx
@@ -1217,7 +1217,7 @@ class AdviceDetailImpl extends React.Component {
                     {
                         ((!approvalRequested && isPublic) || !isPublic) &&
                         <Button
-                                onClick={() => this.props.history.push(`/advisordashboard/updateadvice/${this.props.match.params.id}`)}
+                                onClick={() => this.props.history.push(`/dashboard/updateadvice/${this.props.match.params.id}`)}
                                 className={className}
                                 style={buttonStyle}
                         >
