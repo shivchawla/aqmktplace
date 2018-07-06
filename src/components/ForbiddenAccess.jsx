08c71681339d5bfcdf0bd8e549263cde1e99bc4f
Saurav Biswas <saru.sreyo@gmail.com>
2018-07-06 19:59:24 +0530

    Added GoBack button to warning pages

--- src/components/ForbiddenAccess.jsx
+++ src/components/ForbiddenAccess.jsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
-import {Row} from 'antd';
+import {Row, Button} from 'antd';
 
 export default class ForbiddenAccess extends React.Component {
     render() {
         return (
             <Row type="flex" align="middle" justify="center" style={{height: '100%'}}>
-                <h1 
-                        style={{textAlign: 'left', position: 'absolute', top: '50%', fontSize: '40px', fontWeight: '700'}}
-                >
-                    403 Forbidden
-                    <span style={{fontSize: '20px', fontWeight: '400', marginLeft: '5px'}}>Access Denied.</span>
-                </h1>
+                <div style={{textAlign: 'center', position: 'absolute', top: '50%'}}>
+                    <h1 
+                            style={{fontSize: '40px', fontWeight: '700'}}
+                    >
+                        403 Forbidden
+                        <span style={{fontSize: '20px', fontWeight: '400', marginLeft: '5px'}}>Access Denied.</span>
+                    </h1>
+                    <Button type="primary" onClick={() => this.props.history.push('/advice')}>GO TO ADVICES</Button>
+                </div>
             </Row>
         );
     }
--- src/components/NoIternetAccess.jsx
+++ src/components/NoIternetAccess.jsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
-import {Row} from 'antd';
+import {Row, Button} from 'antd';
 export default class NoIternetAccess extends React.Component {
     render() {
         return (
             <Row type="flex" align="middle" justify="center" style={{height: '100%'}}>
-                <h1 
-                        style={{textAlign: 'left', position: 'absolute', top: '50%', fontSize: '40px', fontWeight: '700'}}
-                >
-                    Network Error
-                    <span style={{fontSize: '20px', fontWeight: '400', marginLeft: '5px'}}>Something Went Wrong.</span>
-                </h1>
+                <div style={{textAlign: 'center', position: 'absolute', top: '50%'}}>
+                    <h1 
+                            style={{fontSize: '40px', fontWeight: '700'}}
+                    >
+                        Network Error
+                        <span style={{fontSize: '20px', fontWeight: '400', marginLeft: '5px'}}>Something Went Wrong.</span>
+                    </h1>
+                    <Button type="primary" onClick={() => this.props.history.goBack()}>GO BACK</Button>
+                </div>
             </Row>
         );
     }
