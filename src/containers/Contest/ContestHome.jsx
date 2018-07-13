96b4dea34f134e3293d35d7bd1b4608b2ca3d13b
Saurav Biswas <saru.sreyo@gmail.com>
2018-07-13 10:26:27 +0530

    Moved constants to ./constants

--- src/containers/Contest/ContestHome.jsx
+++ src/containers/Contest/ContestHome.jsx
@@ -1,18 +1,9 @@
 import * as React from 'react';
 import {Row, Col, Button, Tabs, Table, Tag, Icon} from 'antd';
 import {primaryColor, verticalBox, horizontalBox} from '../../constants';
-import enterContestIcon from '../../assets/enter-contest.svg';
+import {metrics, faqs, howItWorksContents, prizes, criterias} from './constants';
 
 const TabPane = Tabs.TabPane;
-
-const metrics = [
-    {header: 'Total Return', content: 'The Total Return of your portfolio'},
-    {header: 'Annual Return', content: 'The Annual Return of your portfolio'},
-    {header: 'Volatility', content: 'The Volatility of your portfolio'},
-    {header: 'Sharpe', content: 'The Sharpe of your portfolio'},
-    {header: 'Beta', content: 'The Beta of your portfolio'},
-    {header: 'Alpha', content: 'The Alpha of your portfolio'}
-];
 
 export default class ContestHome extends React.Component {
     constructor(props) {
@@ -53,16 +44,11 @@ export default class ContestHome extends React.Component {
         );
     }
 
-    renderHowItWorks = () => {
-        const contents = [
-            {image: enterContestIcon, header: 'Enter Contest', content: 'Write an algorithm, and submit it to the contest. Submissions are entered into the contest starting at the next market open.'},
-            {image: enterContestIcon, header: 'Enter Contest', content: 'Write an algorithm, and submit it to the contest. Submissions are entered into the contest starting at the next market open.'},
-            {image: enterContestIcon, header: 'Enter Contest', content: 'Write an algorithm, and submit it to the contest. Submissions are entered into the contest starting at the next market open.'}
-        ];
+    renderHowItWorks = () => {   
         return (
             <Row type="flex" justify="space-between" style={{marginTop: '50px'}}>
                 {
-                    contents.map((item, index) => {
+                    howItWorksContents.map((item, index) => {
                         return <HowItWorksCard key={index} {...item} />
                     })
                 }
@@ -71,19 +57,6 @@ export default class ContestHome extends React.Component {
     }
 
     renderPrizeList = () => {
-        const containerStyle = {};
-        const prizes = [
-            {key: 1, rank: '1st Place', reward: '10000'},
-            {key: 2,rank: '2nd Place', reward: '10000'},
-            {key: 3,rank: '3rd Place', reward: '10000'},
-            {key: 4,rank: '4th Place', reward: '10000'},
-            {key: 5,rank: '5th Place', reward: '10000'},
-            {key: 6,rank: '6th Place', reward: '10000'},
-            {key: 7,rank: '7th Place', reward: '10000'},
-            {key: 8,rank: '8th Place', reward: '10000'},
-            {key: 9,rank: '9th Place', reward: '10000'},
-            {key: 10,rank: '10th Place', reward: '10000'},
-        ];
         const columns = [
             {
                 title: 'PLACE',
@@ -115,17 +88,6 @@ export default class ContestHome extends React.Component {
     }
 
     renderCriteriaList = () => {
-        const criterias = [
-            {header: 'Returns', content: 'Your algorithm must make money.'},
-            {header: 'Long/Short', content: 'Your algorithm must make money.'},
-            {header: 'Long/Short', content: 'Your algorithm must make money.'},
-            {header: 'Long/Short', content: 'Your algorithm must make money.'},
-            {header: 'Long/Short', content: 'Your algorithm must make money.'},
-            {header: 'Long/Short', content: 'Your algorithm must make money.'},
-            {header: 'Long/Short', content: 'Your algorithm must make money.'},
-            {header: 'Long/Short', content: 'Your algorithm must make money.'},
-        ];
-
         return (
             <Row style={{marginTop: '20px'}}>
                 {
@@ -190,20 +152,7 @@ export default class ContestHome extends React.Component {
         );
     }
 
-    renderFAQ = () => {
-        const faqs = [
-            {header: 'How do I submit my algorithm?', content: 'Go to your algorithm library and choose an algorithm. Run a backtest for that algorithm and then click Enter Contest.'},
-            {header: 'Can I submit multiple algorithms?', content: 'Yes, you can submit up to 3 algorithms at a time. You may also stop any of your submitted algorithms to make room for another.'},
-            {header: 'How do I submit my algorithm?', content: 'Go to your algorithm library and choose an algorithm. Run a backtest for that algorithm and then click Enter Contest.'},
-            {header: 'Can I submit multiple algorithms?', content: 'Yes, you can submit up to 3 algorithms at a time. You may also stop any of your submitted algorithms to make room for another.'},
-            {header: 'How do I submit my algorithm?', content: 'Go to your algorithm library and choose an algorithm. Run a backtest for that algorithm and then click Enter Contest.'},
-            {header: 'Can I submit multiple algorithms?', content: 'Yes, you can submit up to 3 algorithms at a time. You may also stop any of your submitted algorithms to make room for another.'},
-            {header: 'How do I submit my algorithm?', content: 'Go to your algorithm library and choose an algorithm. Run a backtest for that algorithm and then click Enter Contest.'},
-            {header: 'Can I submit multiple algorithms?', content: 'Yes, you can submit up to 3 algorithms at a time. You may also stop any of your submitted algorithms to make room for another.'},
-            {header: 'How do I submit my algorithm?', content: 'Go to your algorithm library and choose an algorithm. Run a backtest for that algorithm and then click Enter Contest.'},
-            {header: 'Can I submit multiple algorithms?', content: 'Yes, you can submit up to 3 algorithms at a time. You may also stop any of your submitted algorithms to make room for another.'},
-        ];
-        
+    renderFAQ = () => {        
         return (
             <Row>
                 {
---
+++ src/containers/Contest/constants.js
@@ -0,0 +1,53 @@
+import enterContestIcon from '../../assets/enter-contest.svg';
+
+export const metrics = [
+    {header: 'Total Return', content: 'The Total Return of your portfolio'},
+    {header: 'Annual Return', content: 'The Annual Return of your portfolio'},
+    {header: 'Volatility', content: 'The Volatility of your portfolio'},
+    {header: 'Sharpe', content: 'The Sharpe of your portfolio'},
+    {header: 'Beta', content: 'The Beta of your portfolio'},
+    {header: 'Alpha', content: 'The Alpha of your portfolio'}
+];
+
+export const faqs = [
+    {header: 'How do I submit my algorithm?', content: 'Go to your algorithm library and choose an algorithm. Run a backtest for that algorithm and then click Enter Contest.'},
+    {header: 'Can I submit multiple algorithms?', content: 'Yes, you can submit up to 3 algorithms at a time. You may also stop any of your submitted algorithms to make room for another.'},
+    {header: 'How do I submit my algorithm?', content: 'Go to your algorithm library and choose an algorithm. Run a backtest for that algorithm and then click Enter Contest.'},
+    {header: 'Can I submit multiple algorithms?', content: 'Yes, you can submit up to 3 algorithms at a time. You may also stop any of your submitted algorithms to make room for another.'},
+    {header: 'How do I submit my algorithm?', content: 'Go to your algorithm library and choose an algorithm. Run a backtest for that algorithm and then click Enter Contest.'},
+    {header: 'Can I submit multiple algorithms?', content: 'Yes, you can submit up to 3 algorithms at a time. You may also stop any of your submitted algorithms to make room for another.'},
+    {header: 'How do I submit my algorithm?', content: 'Go to your algorithm library and choose an algorithm. Run a backtest for that algorithm and then click Enter Contest.'},
+    {header: 'Can I submit multiple algorithms?', content: 'Yes, you can submit up to 3 algorithms at a time. You may also stop any of your submitted algorithms to make room for another.'},
+    {header: 'How do I submit my algorithm?', content: 'Go to your algorithm library and choose an algorithm. Run a backtest for that algorithm and then click Enter Contest.'},
+    {header: 'Can I submit multiple algorithms?', content: 'Yes, you can submit up to 3 algorithms at a time. You may also stop any of your submitted algorithms to make room for another.'},
+];
+
+export const howItWorksContents = [
+    {image: enterContestIcon, header: 'Enter Contest', content: 'Write an algorithm, and submit it to the contest. Submissions are entered into the contest starting at the next market open.'},
+    {image: enterContestIcon, header: 'Enter Contest', content: 'Write an algorithm, and submit it to the contest. Submissions are entered into the contest starting at the next market open.'},
+    {image: enterContestIcon, header: 'Enter Contest', content: 'Write an algorithm, and submit it to the contest. Submissions are entered into the contest starting at the next market open.'}
+];
+
+export const prizes = [
+    {key: 1, rank: '1st Place', reward: '10000'},
+    {key: 2,rank: '2nd Place', reward: '10000'},
+    {key: 3,rank: '3rd Place', reward: '10000'},
+    {key: 4,rank: '4th Place', reward: '10000'},
+    {key: 5,rank: '5th Place', reward: '10000'},
+    {key: 6,rank: '6th Place', reward: '10000'},
+    {key: 7,rank: '7th Place', reward: '10000'},
+    {key: 8,rank: '8th Place', reward: '10000'},
+    {key: 9,rank: '9th Place', reward: '10000'},
+    {key: 10,rank: '10th Place', reward: '10000'},
+];
+
+export const criterias = [
+    {header: 'Returns', content: 'Your algorithm must make money.'},
+    {header: 'Long/Short', content: 'Your algorithm must make money.'},
+    {header: 'Long/Short', content: 'Your algorithm must make money.'},
+    {header: 'Long/Short', content: 'Your algorithm must make money.'},
+    {header: 'Long/Short', content: 'Your algorithm must make money.'},
+    {header: 'Long/Short', content: 'Your algorithm must make money.'},
+    {header: 'Long/Short', content: 'Your algorithm must make money.'},
+    {header: 'Long/Short', content: 'Your algorithm must make money.'},
+];
