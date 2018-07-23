44b6ee1819490a6dcaf861d419f55d6bd0007f05
Saurav Biswas <saru.sreyo@gmail.com>
2018-07-23 16:12:35 +0530

    Modified portfolio for Mobile

--- src/containers/Contest/CreateAdvice/Portfolio.jsx
+++ src/containers/Contest/CreateAdvice/Portfolio.jsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
 import _ from 'lodash';
+import Media from 'react-media';
 import {Row, Col, Button, Modal, Spin, Select, Tooltip, Badge} from 'antd';
 import {metricColor, horizontalBox, primaryColor, verticalBox} from '../../../constants';
 import {generateColorData} from '../../../utils';
 import {AqStockTableMod} from '../../../components/AqStockTableMod';
+import PortfolioList from './Mobile/PortfolioList';
 import {benchmarks} from '../../../constants/benchmarks';
 import MyChartNew from '../../MyChartNew';
 import {HighChartNew} from '../../../components/HighChartNew';
 
 const Option = Select.Option;
-
+const screenSize = {mobile: '600px', desktop: '601px'};
 export class Portfolio extends React.Component {
     constructor(props) {
         super(props);
@@ -78,21 +80,6 @@ export class Portfolio extends React.Component {
         );
     }
 
-    renderBenchmarkDropdown = () => (
-        <Select 
-                defaultValue={this.state.selectedBenchmark} 
-                style={{width: '150px'}}
-                onChange={value => this.loadPerformance(value)}
-                disabled={true}
-        >
-            {
-                this.state.benchmarks.map((benchmark, index) => (
-                    <Option key={index} value={benchmark}>{benchmark}</Option>
-                ))
-            }
-        </Select>
-    )
-
     togglePerformanceModal = () => {
         if (!this.state.modal.performance) {
             this.loadPerformance(this.state.selectedBenchmark);
@@ -102,43 +89,7 @@ export class Portfolio extends React.Component {
             performance: !this.state.modal.performance
         }});
     }
-
-    toggleCompositionModal = () => {
-        this.setState({
-            modal: {
-                ...this.state.modal,
-                composition: !this.state.modal.composition
-            }
-        });
-    }
-
-    renderPositions = () => {
-        const data = this.processDataForPieChart();
-        return (
-            <Row type="flex" justify="space-around">
-                {
-                    data.map((position, index) => {
-                        return (
-                            <Col 
-                                    key={index}
-                                    span={8} 
-                                    style={{
-                                        display: 'flex',
-                                        flexDirection: 'column',
-                                        marginBottom: '20px',
-                                        textAlign: 'center'
-                                    }}
-                            >
-                                <h3 style={{color: '#4a4a4a', fontSize: '16px', fontWeight: 300}}>{position.y} %</h3>
-                                <h3 style={{color: position.color, fontSize: '14px', fontWeight: 400}}>{position.name}</h3>
-                            </Col>
-                        );
-                    })
-                }
-            </Row>
-        );
-    }
-
+    
     renderPerformanceModal = () => {
         return (
             <Modal
@@ -159,11 +110,6 @@ export class Portfolio extends React.Component {
                                 <Col span={24} style={{marginTop: '5px'}}>
                                     {this.renderMetrics()}
                                 </Col>
-
-                                {/* <Col span={24} style={{display: 'flex', justifyContent: 'flex-end'}}>
-                                    {this.renderBenchmarkDropdown()}
-                                </Col> */}
-
                                 <Col span={24}>
                                     <MyChartNew 
                                             series={this.state.highStockSeries} 
@@ -172,100 +118,10 @@ export class Portfolio extends React.Component {
                                 </Col>
                             </Row>
                         </Col>
-                        {/* <Col 
-                                span={10} 
-                                style={{
-                                    display: 'flex', 
-                                    flexDirection: 'column', 
-                                    justifyContent: 'center',
-                                    alignItems: 'center',
-                                    padding: '0 10px',
-                                    overflow: 'hidden',
-                                    overflowY: 'scroll',
-                                    height: '100%'
-                                }}
-                        >
-                            <Row>
-                                <Col span={24}>
-                                    <HighChartNew 
-                                        series={[{name: 'Portfolio Composition', data: this.processDataForPieChart(this.props.data)}]}
-                                    />
-                                </Col>
-                                <Col span={24}>
-                                    {this.renderPositions()}
-                                </Col>
-                            </Row>
-                        </Col> */}
                     </Row>
                 </Spin>
             </Modal>
         );
-    }
-
-    renderCompositionModal = () => {
-        const series = [];
-        
-        return (
-            <Modal
-                    title="Composition"
-                    onOk={this.toggleCompositionModal}
-                    onCancel={this.toggleCompositionModal}
-                    visible={this.state.modal.composition}
-            >
-                <Row>
-                    <Col span={24}>
-                        <HighChartNew 
-                            series={[{name: 'Portfolio Composition', data: this.processDataForPieChart(this.props.data)}]}
-                        />
-                    </Col>
-                </Row>
-            </Modal>
-        );
-    }
-
-    checkTickerForDuplications = (data, ticker) => {
-        const duplicationIndexes = [];
-        const nData = data.filter((dataItem, index) => {
-            if (dataItem.ticker === ticker) {
-                duplicationIndexes.push(index);
-            }
-            return dataItem.ticker === ticker
-        });
-
-        return {indexes: duplicationIndexes, length: nData.length -1}
-    }
-
-    processDataForPieChart = () => {
-        let {data = []} = this.props;
-        data = data.filter(item => item.shares > 0);
-        const tickers = data.map(item => item.symbol);
-        const colorData = generateColorData(tickers);
-        let nData = data.map((item, index) => {
-            const duplicateData = this.checkTickerForDuplications(data, item.ticker);
-            let duplicateIndexes = duplicateData.indexes; // [0, 1, 2]
-            const duplicateLength = duplicateData.length;
-            let duplicateTotal = 0;
-            // Removing the current index from the duplicate index array
-            duplicateIndexes = duplicateIndexes.filter(duplicateIndex => {
-                return duplicateIndex !== index
-            });
-            if (duplicateLength > 0) {
-                duplicateIndexes.map(duplicateIndex => {
-                    duplicateTotal += data[duplicateIndex].weight;
-                });
-                duplicateTotal += item.weight || 0;
-            } else {
-                duplicateTotal = item.weight || 0;
-            }
-            
-            return {
-                name: _.get(item, 'ticker', null),
-                y: Number(duplicateTotal.toFixed(2)),
-                color: colorData[_.get(item, 'ticker', null)]
-            }
-        });
-
-        return _.uniqBy(nData, 'name');
     }
 
     shouldComponentUpdate(nextProps, nextState) {
@@ -276,61 +132,112 @@ export class Portfolio extends React.Component {
         return false;
     }
 
+    renderPortfolioTable = () => {
+        return (
+            <Col span={24} style={{marginTop: '20px'}}>
+                <AqStockTableMod 
+                    onChange = {this.props.onChange}
+                    data={this.props.data}
+                    isUpdate={this.props.isUpdate}
+                    benchmark={this.props.benchmark}
+                    stockSearchFilters={this.props.stockSearchFilters}
+                />
+            </Col>
+        );
+    }
+
+    renderPortfolioList = () => {
+        return (
+            <PortfolioList />
+        );
+    }
+
+    renderPortfolio = () => {
+        return (
+            <React.Fragment>
+                <Media 
+                    query="(max-width: 600px)"
+                    render={() => this.renderPortfolioList()}
+                />
+                <Media 
+                    query="(min-width: 601px)"
+                    render={() => this.renderPortfolioTable()}
+                />
+            </React.Fragment>
+        );
+    }
+
+    renderActionButtonsDesktop = () => {
+        return (
+            <div style={{
+                    position: 'absolute', 
+                    right: '0px', 
+                    top: '25px',
+                    zIndex: 20
+                }}
+            >
+                <Button
+                        style={{
+                            marginLeft: '20px'
+                        }} 
+                        onClick={this.togglePerformanceModal} 
+                        type="secondary"
+                        // disabled={this.props.verifiedPositions.length < 1}
+                        disabled={this.props.data.length < 1}
+                        icon="area-chart"
+                >
+                    PERFORMANCE
+                </Button>
+                {
+                    this.props.data.length > 0 &&
+                    <Tooltip title="Search Stocks" placement="top">
+                        <Button 
+                                style={{marginLeft: '20px'}} 
+                                type="primary" 
+                                icon="plus-circle-o"
+                                onClick={this.props.toggleBottomSheet}
+                                disabled={this.props.benchmark === null}
+                        >
+                            ADD STOCKS
+                        </Button>
+                    </Tooltip>
+                }
+            </div>  
+        );
+    }
+
+    renderButtonsMobile = () => {
+
+    }
+
+    renderActionButtons = () => {
+        return (
+            <React.Fragment>
+                <Media />
+            </React.Fragment>
+        );
+    }
+
+    renderBenchmarkDropdownDesktop = () => {
+        return (
+            <Col>
+                {
+                    this.props.benchmark === null &&
+                    <h3 style={{color: metricColor.negative}}>
+                        Please choose a benchmark for your Portfolio
+                    </h3>
+                }
+            </Col>
+        );
+    }
+
     render() {
         return (
             <Row style={{display: 'block'}} type="flex">
                 {this.renderPerformanceModal()}
-                <Col>
-                    {
-                        this.props.benchmark === null &&
-                        <h3 style={{color: metricColor.negative}}>
-                            Please choose a benchmark for your Portfolio
-                        </h3>
-                    }
-                </Col>
-                <div style={{
-                            position: 'absolute', 
-                            right: '0px', 
-                            top: '25px',
-                            zIndex: 20
-                        }}
-                >
-                    <Button
-                            style={{
-                                marginLeft: '20px'
-                            }} 
-                            onClick={this.togglePerformanceModal} 
-                            type="secondary"
-                            // disabled={this.props.verifiedPositions.length < 1}
-                            disabled={this.props.data.length < 1}
-                            icon="area-chart"
-                    >
-                        PERFORMANCE
-                    </Button>
-                    {
-                        this.props.data.length > 0 &&
-                        <Tooltip title="Search Stocks" placement="top">
-                            <Button 
-                                    style={{marginLeft: '20px'}} 
-                                    type="primary" 
-                                    icon="plus-circle-o"
-                                    onClick={this.props.toggleBottomSheet}
-                                    disabled={this.props.benchmark === null}
-                            >
-                                ADD STOCKS
-                            </Button>
-                        </Tooltip>
-                    }
-                </div>
-                <Col span={24} style={{marginTop: '20px'}}>
-                    <AqStockTableMod 
-                        onChange = {this.props.onChange}
-                        data={this.props.data}
-                        isUpdate={this.props.isUpdate}
-                        benchmark={this.props.benchmark}
-                        stockSearchFilters={this.props.stockSearchFilters}
-                    />
-                </Col>
+                {this.renderBenchmarkDropdownDesktop()}
+                {this.renderActionButtonsDesktop()}
+                {this.renderPortfolio()}
                 {
                     this.props.data.length === 0 &&
                     <Col span={24} style={{...verticalBox, marginTop: '40px'}}>
